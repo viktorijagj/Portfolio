a6d30cf9ff805f8f9af77ba95e3538a894da8493
viktorija <viktorija_1102992@yahoo.com>
2023-01-18 00:22:49 +0100

    darkmode added

--- script/main.js
+++ script/main.js
@@ -1,3 +1,32 @@
+// Darkmode js
+const options = {
+//   top: '64px',
+  bottom:'unset', // default: '32px'
+  right: '32px', // default: '32px'
+  left: 'unset', // default: 'unset'
+  time: '0.3s', // default: '0.3s'
+  mixColor: '#fff', // default: '#fff'
+  backgroundColor: '#fff',  // default: '#fff'
+  buttonColorDark: '#100f2c',  // default: '#100f2c'
+  buttonColorLight: '#fff', // default: '#fff'
+  saveInCookies: true, // default: true,
+  label: '&#128994;', // default: ''
+  autoMatchOsTheme: true // default: true
+}
+
+const darkmode = new Darkmode(options);
+darkmode.showWidget();
+
+// Add active link class
+const links = document.getElementsByClassName("tab-link");
+for(var i = 0; i < links.length; i++){
+links[i].addEventListener("click", function(){
+    const current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace("active", "");
+    this.className += " active";
+});
+}
+
 // Scroll reveal
 window.sc = ScrollReveal({
     origin:'top',
@@ -7,6 +36,7 @@ window.sc = ScrollReveal({
      reset:true
 });
 sc.reveal('.main-header, .carousel');
+sc.reveal('.personal-info', {delay:800, origin:'left',interval:100});
 sc.reveal('.education, .form', {delay:500, origin:'left',interval:100});
 sc.reveal('.info, .social',{delay:500, origin:'right',interval:100});
 sc.reveal('.progress-bar-container',{delay:600, origin:'top',interval:100})
